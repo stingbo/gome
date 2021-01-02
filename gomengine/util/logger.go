89e7375fbd2af8c2740871e92b5bbbb25eb2612b
wanlianbo <lianbo.wan@gmail.com>
2021-01-02 20:09:20 +0800

    修改错误单字

--- gomengine/util/logger.go
+++ gomengine/util/logger.go
@@ -15,7 +15,7 @@ func init() {
 	//日志输出文件
 	file, err := os.OpenFile("order.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0777)
 	if err != nil {
-		log.Fatalln("Faild to open error logger file:", err)
+		log.Fatalln("Failed to open error logger file:", err)
 	}
 	//自定义日志格式
 	Info = log.New(io.MultiWriter(file, os.Stderr), "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
