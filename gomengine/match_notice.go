e269e8754995d192127c420381ea886d0fc8e0d5
wanlianbo <lianbo.wan@gmail.com>
2021-01-02 19:48:09 +0800

    修改撮合队列的启动方式，使用指定交易对的方式进行撮合与通知

---
+++ gomengine/match_notice.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"errors"
+	"github.com/urfave/cli/v2"
+	"gome/gomengine/engine"
+	"log"
+	"os"
+)
+
+func main() {
+	app := &cli.App{
+		Name: "notice",
+		Usage: "消费撮合成功后的结果",
+		Action: func(c *cli.Context) error {
+			symbol := c.Args().Get(0)
+			if symbol == "" {
+				return errors.New("请输入需要消费的队列名称")
+			}
+			mq := engine.NewSimpleRabbitMQ(symbol)
+			mq.ConsumeMatchOrder()
+
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
