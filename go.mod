d571648ac4abd89e8eb85762cd53de6022c79619
wanlianbo <lianbo.wan@gmail.com>
2021-01-06 23:23:30 +0800

    使用zap记录日志

--- go.mod
+++ go.mod
@@ -4,12 +4,20 @@ go 1.15
 
 require (
 	github.com/go-redis/redis/v8 v8.0.0-beta.8
-	github.com/golang/protobuf v1.4.2
+	github.com/golang/protobuf v1.4.3
+	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2
 	github.com/shopspring/decimal v1.2.0
 	github.com/streadway/amqp v1.0.0
 	github.com/urfave/cli/v2 v2.3.0
-	golang.org/x/net v0.0.0-20190923162816-aa69164e4478
-	google.golang.org/grpc v1.31.1
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.16.0
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b
+	golang.org/x/sys v0.0.0-20210105210732-16f7687f5001 // indirect
+	golang.org/x/text v0.3.4 // indirect
+	google.golang.org/genproto v0.0.0-20210105202744-fe13368bc0e1 // indirect
+	google.golang.org/grpc v1.34.0
+	google.golang.org/grpc/examples v0.0.0-20210105215357-f4a20d2f414f // indirect
 	google.golang.org/protobuf v1.25.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776
 )
--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	_ "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	grpc_opentracing "github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"gome/api"
 	"gome/engine"
 	rpc "gome/grpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"gopkg.in/natefinch/lumberjack.v2"
 	"log"
 )
 
@@ -19,11 +28,61 @@ func main() {
 	rpcListener := rpc.NewRpcListener()
 	listener := rpcListener.Listener
 
-	rpcServer := grpc.NewServer()
+	//rpcServer := grpc.NewServer()
+	rpcServer := grpc.NewServer(
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
+			grpc_ctxtags.StreamServerInterceptor(),
+			grpc_opentracing.StreamServerInterceptor(),
+			//grpc_prometheus.StreamServerInterceptor,
+			grpc_zap.StreamServerInterceptor(ZapInterceptor()),
+			//grpc_auth.StreamServerInterceptor(myAuthFunction),
+			grpc_recovery.StreamServerInterceptor(),
+		)),
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			grpc_ctxtags.UnaryServerInterceptor(),
+			grpc_opentracing.UnaryServerInterceptor(),
+			//grpc_prometheus.UnaryServerInterceptor,
+			grpc_zap.UnaryServerInterceptor(ZapInterceptor()),
+			//grpc_auth.UnaryServerInterceptor(myAuthFunction),
+			grpc_recovery.UnaryServerInterceptor(),
+		)),
+	)
 	api.RegisterOrderServer(rpcServer, &engine.Order{})
 	api.RegisterPoolServer(rpcServer, &engine.Pool{})
 	reflection.Register(rpcServer)
 	if err := rpcServer.Serve(listener); err != nil {
 		log.Fatalln("服务启动失败")
 	}
+}
+
+func ZapInterceptor() *zap.Logger {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatalf("failed to initialize zap logger: %v", err)
+	}
+	grpc_zap.ReplaceGrpcLoggerV2(logger)
+
+	return logger
+}
+
+// ZapInterceptor 返回zap.logger实例(把日志写到文件中)
+func ZapFileInterceptor() *zap.Logger {
+	w := zapcore.AddSync(&lumberjack.Logger{
+		Filename:  "log/debug.log",
+		MaxSize:   1024, //MB
+		LocalTime: true,
+	})
+
+	config := zap.NewProductionEncoderConfig()
+	config.EncodeTime = zapcore.ISO8601TimeEncoder
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(config),
+		w,
+		zap.NewAtomicLevel(),
+	)
+
+	logger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
+	grpc_zap.ReplaceGrpcLoggerV2(logger)
+
+	return logger
 }
